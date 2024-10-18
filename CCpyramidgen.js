99cd55e904bb8d780909031dc2bc5604dfa2bdc0
Jack Pake <99498154+jackpake04@users.noreply.github.com>
2024-10-18 00:34:52 -0400

    Update CCpyramidgen.js

--- CCpyramidgen.js
+++ CCpyramidgen.js
@@ -2,15 +2,23 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(name) {
+  const test = "Testing";  
+  return character + name;
+}
+
+const call = padRow("CamperChan");
+console.log(call);
+
+
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+  rows.push(character.repeat(i + 1))
 }
 
 let result = ""
 
 for (const row of rows) {
-    result = result + row;
-  }
-  
-  console.log(result);
-  //hi
+  result = result + "\n" + row;
+}
+
+console.log(result);
